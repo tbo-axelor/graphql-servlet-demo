bea44967644e193deebff0794876d9f3cb6b485e
Tushar BODARYA <t.bodarya@axelor.com>
2018-09-12 12:45:12 +0530

    implement: features of insert record

--- src/main/java/com/axelor/app/GraphQLEndpoint.java
+++ src/main/java/com/axelor/app/GraphQLEndpoint.java
@@ -2,7 +2,9 @@ package com.axelor.app;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
@@ -19,16 +21,24 @@ import javax.servlet.annotation.WebServlet;
 import graphql.Scalars;
 import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLInputObjectField;
+import graphql.schema.GraphQLInputObjectType;
+import graphql.schema.GraphQLInputType;
 import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLOutputType;
 import graphql.schema.GraphQLSchema;
+import graphql.schema.GraphQLType;
 import graphql.schema.GraphQLTypeReference;
 import graphql.servlet.SimpleGraphQLServlet;
 
 @WebServlet(urlPatterns = "/graphql")
 public class GraphQLEndpoint extends SimpleGraphQLServlet {
+	
 
+	private static Map<String, GraphQLFieldDefinition.Builder> graphqlObject = new HashMap<>();
+	private static Map<String, GraphQLInputObjectField.Builder> graphqlInputObject = new HashMap<>();
+	
 	public GraphQLEndpoint() {
 		super(buildSchema());
 	}
@@ -36,16 +46,21 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 	private static GraphQLSchema buildSchema() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("graphql-java");
 		EntityManager em = emf.createEntityManager();
-		return GraphQLSchema.newSchema().query(getQueryObject(em)).build();
+		return GraphQLSchema.newSchema().query(getQueryObject(em)).mutation(getMutationObject(em)).build();
 
 	}
-	
+
 	private static GraphQLObjectType getQueryObject(EntityManager em) {
-		return GraphQLObjectType.newObject().name("query").fields(em.getMetamodel()
-				.getEntities().stream().map(t -> getFieldDefination(t, em)).collect(Collectors.toList())).build();
+		return GraphQLObjectType.newObject().name("query").fields(em.getMetamodel().getEntities().stream()
+				.map(t -> getFieldDefination(t, em).build()).collect(Collectors.toList())).build();
 	}
-	
-	private static GraphQLOutputType findBasicType(Attribute<?, ?> attr) {
+
+	private static GraphQLObjectType getMutationObject(EntityManager em) {
+		return GraphQLObjectType.newObject().name("mutation").fields(em.getMetamodel().getEntities().stream()
+				.map(t -> getFieldDefination(t, em).argument(getInputObjectArgument(t)).build()).collect(Collectors.toList())).build();
+	}
+
+	private static GraphQLType findBasicType(Attribute<?, ?> attr) {
 		Class<?> type = attr.getJavaType();
 		if (String.class.isAssignableFrom(type)) {
 			return Scalars.GraphQLString;
@@ -64,28 +79,38 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 			return Scalars.GraphQLBigDecimal;
 		} else {
 			throw new UnsupportedOperationException(
-	                "Class could not be mapped to GraphQL: '" + type.getClass().getTypeName() + "'");
+					"Class could not be mapped to GraphQL: '" + type.getClass().getTypeName() + "'");
 		}
 	}
 
 	private static GraphQLOutputType findType(Attribute<?, ?> attr) {
-		if(attr.getPersistentAttributeType() == PersistentAttributeType.BASIC) {
-			return findBasicType(attr);
-		} else if(attr.getPersistentAttributeType() == PersistentAttributeType.ONE_TO_MANY || attr.getPersistentAttributeType() == PersistentAttributeType.MANY_TO_MANY) {
+		if (attr.getPersistentAttributeType() == PersistentAttributeType.BASIC) {
+			return (GraphQLOutputType) findBasicType(attr);
+		} else if (attr.getPersistentAttributeType() == PersistentAttributeType.ONE_TO_MANY
+				|| attr.getPersistentAttributeType() == PersistentAttributeType.MANY_TO_MANY) {
 			Type<?> foreignType = ((PluralAttribute<?, ?, ?>) attr).getElementType();
 			return new GraphQLList(new GraphQLTypeReference(foreignType.getJavaType().getSimpleName()));
-		} else if(attr.getPersistentAttributeType() == PersistentAttributeType.MANY_TO_ONE || attr.getPersistentAttributeType() == PersistentAttributeType.ONE_TO_ONE) {
+		} else if (attr.getPersistentAttributeType() == PersistentAttributeType.MANY_TO_ONE
+				|| attr.getPersistentAttributeType() == PersistentAttributeType.ONE_TO_ONE) {
 			Type<?> foreignType = ((SingularAttribute<?, ?>) attr).getType();
 			return new GraphQLTypeReference(foreignType.getJavaType().getSimpleName());
 		} else {
-			throw new UnsupportedOperationException(
-	                "Attribute could not be mapped to GraphQL: field '" + attr.getDeclaringType().getJavaType().getName() + "' of entity class '" + attr.getJavaMember().getName() + "'");
+			throw new UnsupportedOperationException("Attribute could not be mapped to GraphQL: field '"
+					+ attr.getDeclaringType().getJavaType().getName() + "' of entity class '"
+					+ attr.getJavaMember().getName() + "'");
 		}
 	}
 
-	private static GraphQLFieldDefinition getFieldDefination(EntityType<?> entity, EntityManager entityManager) {
-		return GraphQLFieldDefinition.newFieldDefinition().name(entity.getName()).type(new GraphQLList(getType(entity))).argument(getArgumentList())
-				.dataFetcher(new JpaDataFetcher(entityManager, entity)).build();
+	private static GraphQLFieldDefinition.Builder getFieldDefination(EntityType<?> entity,
+			EntityManager entityManager) {
+		if(graphqlObject.containsKey(entity.getName())) {
+			return graphqlObject.get(entity.getName());
+		} else {
+			GraphQLFieldDefinition.Builder fieldDef =  GraphQLFieldDefinition.newFieldDefinition().name(entity.getName()).type(new GraphQLList(getType(entity)))
+					.argument(getArgumentList()).dataFetcher(new JpaDataFetcher(entityManager, entity));
+			graphqlObject.put(entity.getName(), fieldDef);
+			return fieldDef;
+		}
 	}
 
 	private static GraphQLFieldDefinition getTypeField(Attribute<?, ?> attr) {
@@ -107,10 +132,47 @@ public class GraphQLEndpoint extends SimpleGraphQLServlet {
 	private static GraphQLArgument getFilterArgument() {
 		return GraphQLArgument.newArgument().name("filter").type(Scalars.GraphQLString).build();
 	}
+
 	private static List<GraphQLArgument> getArgumentList() {
 		List<GraphQLArgument> list = new ArrayList<>();
 		list.add(getIdArgument());
-		list.add(getFilterArgument());
+//		list.add(getFilterArgument());
 		return list;
 	}
+	
+	private static GraphQLArgument getInputObjectArgument(EntityType<?> entity) {
+		return GraphQLArgument.newArgument().name("data").type(getInputType(entity)).build();
+		
+	}
+	
+	private static GraphQLInputObjectType getInputType(EntityType<?> entity) {
+		GraphQLInputObjectType.Builder builder = GraphQLInputObjectType.newInputObject().name("Input" + entity.getName());
+		for (Attribute<?, ?> attr : entity.getAttributes()) {
+			builder.field(getInputTypeField(attr));
+		}
+		return builder.build();
+	}
+
+	private static GraphQLInputObjectField getInputTypeField(Attribute<?, ?> attr) {
+		return GraphQLInputObjectField.newInputObjectField().name(attr.getName()).type(findInputType(attr)).build();
+	}
+	
+	private static GraphQLInputType findInputType(Attribute<?, ?> attr) {
+		if (attr.getPersistentAttributeType() == PersistentAttributeType.BASIC) {
+			return (GraphQLInputType) findBasicType(attr);
+		} else if (attr.getPersistentAttributeType() == PersistentAttributeType.ONE_TO_MANY
+				|| attr.getPersistentAttributeType() == PersistentAttributeType.MANY_TO_MANY) {
+			Type<?> foreignType = ((PluralAttribute<?, ?, ?>) attr).getElementType();
+			return new GraphQLList(new GraphQLTypeReference("Input" + foreignType.getJavaType().getSimpleName()));
+		} else if (attr.getPersistentAttributeType() == PersistentAttributeType.MANY_TO_ONE
+				|| attr.getPersistentAttributeType() == PersistentAttributeType.ONE_TO_ONE) {
+			Type<?> foreignType = ((SingularAttribute<?, ?>) attr).getType();
+			return new GraphQLTypeReference("Input" + foreignType.getJavaType().getSimpleName());
+		} else {
+			throw new UnsupportedOperationException("Attribute could not be mapped to GraphQL: field '"
+					+ attr.getDeclaringType().getJavaType().getName() + "' of entity class '"
+					+ attr.getJavaMember().getName() + "'");
+		}
+	}
+	
 }
--- src/main/java/com/axelor/app/JpaDataFetcher.java
+++ src/main/java/com/axelor/app/JpaDataFetcher.java
@@ -1,5 +1,7 @@
 package com.axelor.app;
 
+import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
@@ -8,6 +10,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.EntityType;
+import javax.transaction.Transactional;
 
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
@@ -16,7 +19,7 @@ public class JpaDataFetcher implements DataFetcher {
 
 	private EntityManager entityManager;
 	private EntityType<?> entity;
-	
+
 	public JpaDataFetcher(EntityManager entityManager, EntityType<?> entity) {
 		this.entityManager = entityManager;
 		this.entity = entity;
@@ -28,19 +31,52 @@ public class JpaDataFetcher implements DataFetcher {
 	}
 
 	private TypedQuery<?> getQuery(DataFetchingEnvironment environment) {
-		
-		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-	    CriteriaQuery<?> query = builder.createQuery(entity.getJavaType());
-	    Root<?> entityRoot = query.from(entity);
 
-	    Map<String,Object> args = environment.getArguments();
-	    
-	    if(args.get("id") != null) {
-	    	query.where(builder.equal(entityRoot.get("id"),args.get("id")));
-	    }
-	    	
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<?> query = builder.createQuery(entity.getJavaType());
+		Root<?> entityRoot = query.from(entity);
+
+		Map<String, Object> args = environment.getArguments();
+
+		if (args.get("id") != null) {
+			query.where(builder.equal(entityRoot.get("id"), args.get("id")));
+		} else if (args.get("data") != null) {
+			try {
+				Object obj = persistRecord(environment);
+				Field field = obj.getClass().getDeclaredField("id");    
+				field.setAccessible(true);
+				Object value = field.get(obj);
+				query.where(builder.equal(entityRoot.get("id"), value));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 		return entityManager.createQuery(query);
 
+	}
+
+	private Object persistRecord(DataFetchingEnvironment environment)
+			throws InstantiationException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		Class<?> klass = entity.getJavaType();
+		Object obj = klass.newInstance();
+		Field[] fields = klass.getDeclaredFields();
+		for (Field f : fields) {
+			f.setAccessible(true);
+			if (getValue(environment, f.getName()) != null) {
+				f.set(obj, getValue(environment, f.getName()));
+			}
+		}
+		entityManager.getTransaction().begin();
+		entityManager.persist(obj);
+		entityManager.getTransaction().commit();
+		return obj;
+	}
+
+	private Object getValue(DataFetchingEnvironment env, String name) {
+		LinkedHashMap<String, String> map = new LinkedHashMap<>();
+		map = env.getArgument("data");
+		return map.get(name);
 	}
 
 }
